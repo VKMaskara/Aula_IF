69d41df1f5f42a9a385062c209bc4ab905e91ade
Vitor Kauê <rodriguesvitorkaue@gmail.com>
2025-09-09 22:02:12 -0300

    corrigindo os 15% de bônus

--- salario.py
+++ salario.py
@@ -1,4 +1,4 @@
-# PEGUE O SALARIO DE UM FUNCIONARIO, SE GANHAR MENOS DE 1.200,00 DE AUMENTO DE 151,00, SE TIVER FILHOS GANHA 200,00 POR FILHO
+# PEGUE O SALARIO DE UM FUNCIONARIO, SE GANHAR MENOS DE 1.200,00 DE AUMENTO DE 15%, SE TIVER FILHOS GANHA 200,00 POR FILHO
 #AUTOR: VITOR KAUÊ
 #DATA: 04/09/2025
 
@@ -9,15 +9,11 @@ salario = float(input("Digite o salario do funcionario: "))  # Solicita ao usuá
 filhos = int(input("Digite a quantidade de filhos: "))  # Solicita ao usuário a quantidade de filhos
 aumento = 0  # Inicializa a variável aumento
 
-if salario <= 1200 :  # Verifica se o salário é menor que 1200
-    aumento += 151  # Adiciona 151 ao aumento se o salário for menor que 1200
-else:
-    aumento += 0  # Não adiciona nada ao aumento se o salário for 1200 ou mais
+if salario < 1200:  # Verifica se o salário é menor que 1200
+    aumento += salario * 0.15  # Adiciona 15% ao aumento se o salário for menor que 1200
 
-if filhos > 0 :  # Verifica se o funcionário tem filhos
+if filhos > 0:  # Verifica se o funcionário tem filhos
     aumento += filhos * 200  # Adiciona 200 por cada filho ao aumento
-else:
-    aumento += 0 # Não adiciona nada ao aumento se não tiver filhos
 
 novo_salario = salario + aumento  # Calcula o novo salário somando o salário original e o aumento
 
